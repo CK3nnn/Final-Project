d07b74001fb3d029c24f16d3bcb067342188d81f
CK3nnn <127086478+CK3nnn@users.noreply.github.com>
2024-06-28 12:41:35 +0800

    Delete ByteRage directory

--- ByteRage/community.php
+++
@@ -1,9 +0,0 @@
-<?php include 'header.php'; ?>
-
-<div class="content" style="margin-top: 150px; padding-top: 50px;">
-    <!-- Community content goes here -->
-    <h1>Community Page</h1>
-    <p>Welcome to the community page!</p>
-</div>
-
-<?php include 'footer.php'; ?>
